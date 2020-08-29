c7baca3e6fc81c599c1b1c5eeb426a0efe1b1295
HunterChou1993 <1016642895@qq.com>
2020-08-29 11:22:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+  @import "assets/css/reset.css";
+  @import "assets/css/font.css";
+  #app {
+    width: 100%;
+    height: 3000px;
+    background-color: #DDD;
+  }
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import {
+  Button,
+  Container,
+  Main,
+  Aside,
+  Input,
+  Icon
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Input)
+Vue.use(Icon)
